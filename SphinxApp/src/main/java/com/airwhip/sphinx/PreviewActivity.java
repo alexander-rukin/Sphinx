0edfc97295e878ab73f113c9681b1b075388dee0
Alexander Rukin <a-rukin@yandex-team.ru>
2014-04-14 22:10:01 +0400

    preview activity

--- SphinxApp/src/main/java/com/airwhip/sphinx/PreviewActivity.java
+++ SphinxApp/src/main/java/com/airwhip/sphinx/PreviewActivity.java
@@ -10,8 +10,12 @@ import android.widget.TextView;
 import com.airwhip.sphinx.parser.Characteristic;
 import com.vk.sdk.VKUIHelper;
 
+import org.w3c.dom.Text;
+
 
 public class PreviewActivity extends Activity {
+
+    private final static int SCREEN_MULTIPLIER = 2;
 
     TextView genderText;
 
@@ -20,6 +24,8 @@ public class PreviewActivity extends Activity {
 
     ImageView maleValueImage;
     ImageView femaleValueImage;
+
+    TextView smthText;
 
     TextView ageText;
 
@@ -38,20 +44,24 @@ public class PreviewActivity extends Activity {
         maleValueImage = (ImageView) findViewById(R.id.maleValueImage);
         femaleValueImage = (ImageView) findViewById(R.id.femaleValueImage);
 
+        smthText = (TextView) findViewById(R.id.smthText);
+
         ageText = (TextView) findViewById(R.id.ageText);
 
         continueButton = (TextView) findViewById(R.id.continueButton);
 
         if (Characteristic.isMale()) {
             genderText.setText(getString(R.string.male));
+            smthText.setText(String.format(getString(R.string.smth_text), getString(R.string.male).toLowerCase()));
         } else {
             genderText.setText(getString(R.string.female));
+            smthText.setText(String.format(getString(R.string.smth_text), getString(R.string.female).toLowerCase()));
         }
 
         maleValueText.setText(Characteristic.getMale() + "%");
         femaleValueText.setText(Characteristic.getFemale() + "%");
-        maleValueImage.getLayoutParams().width = Characteristic.getMale();
-        femaleValueImage.getLayoutParams().width = Characteristic.getFemale();
+        maleValueImage.getLayoutParams().width = Characteristic.getMale() * SCREEN_MULTIPLIER;
+        femaleValueImage.getLayoutParams().width = Characteristic.getFemale() * SCREEN_MULTIPLIER;
         ageText.setText(String.valueOf(Characteristic.getAge()));
 
         continueButton.setOnClickListener(new View.OnClickListener() {
